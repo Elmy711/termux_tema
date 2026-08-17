6715c22d6abf61c8d1f9e3fc71072546e8865d26
my🫶el_711 <elmiya0711@gmail.com>
2026-08-17 19:55:56 +0700

    update

--- tema7.py
+++ tema7.py
@@ -101,7 +101,7 @@ def generate_fish_prompt(colors):
 # BEGIN PROMPT CUSTOM ELMY0711
 function fish_prompt
     set_color {c6}
-    echo -n (date "+%a %b %d %H:%M:%S")
+    echo -n (date "+%b %d %H:%M:%S")
     echo ""
     set_color {c5}
     echo -n "╭─"
